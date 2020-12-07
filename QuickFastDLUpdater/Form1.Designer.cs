000b2d3c419427e4e59416c049464a63853f6a76
alexjons <alexjons@kth.se>
2020-12-07 12:56:02 +0100

    Add project files.

---
+++ QuickFastDLUpdater/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace QuickFastDLUpdater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelStatusHeader = new System.Windows.Forms.Label();
+            this.labelFastDLPath = new System.Windows.Forms.Label();
+            this.labelCSGOPath = new System.Windows.Forms.Label();
+            this.linkLabelGitHub = new System.Windows.Forms.LinkLabel();
+            this.linkLabelSteam = new System.Windows.Forms.LinkLabel();
+            this.textBoxServerpath = new System.Windows.Forms.TextBox();
+            this.textBoxFastDLpath = new System.Windows.Forms.TextBox();
+            this.btnBrowseServer = new System.Windows.Forms.Button();
+            this.btnBrowseFastDL = new System.Windows.Forms.Button();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.textBoxPrefix = new System.Windows.Forms.TextBox();
+            this.labelPrefix = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelStatusHeader
+            // 
+            this.labelStatusHeader.AutoSize = true;
+            this.labelStatusHeader.Location = new System.Drawing.Point(12, 394);
+            this.labelStatusHeader.Name = "labelStatusHeader";
+            this.labelStatusHeader.Size = new System.Drawing.Size(40, 13);
+            this.labelStatusHeader.TabIndex = 0;
+            this.labelStatusHeader.Text = "Status:";
+            // 
+            // labelFastDLPath
+            // 
+            this.labelFastDLPath.AutoSize = true;
+            this.labelFastDLPath.Location = new System.Drawing.Point(9, 125);
+            this.labelFastDLPath.Name = "labelFastDLPath";
+            this.labelFastDLPath.Size = new System.Drawing.Size(65, 13);
+            this.labelFastDLPath.TabIndex = 1;
+            this.labelFastDLPath.Text = "FastDL path";
+            // 
+            // labelCSGOPath
+            // 
+            this.labelCSGOPath.AutoSize = true;
+            this.labelCSGOPath.Location = new System.Drawing.Point(9, 59);
+            this.labelCSGOPath.Name = "labelCSGOPath";
+            this.labelCSGOPath.Size = new System.Drawing.Size(98, 13);
+            this.labelCSGOPath.TabIndex = 2;
+            this.labelCSGOPath.Text = "CS:GO Server path";
+            // 
+            // linkLabelGitHub
+            // 
+            this.linkLabelGitHub.AutoSize = true;
+            this.linkLabelGitHub.Location = new System.Drawing.Point(353, 409);
+            this.linkLabelGitHub.Name = "linkLabelGitHub";
+            this.linkLabelGitHub.Size = new System.Drawing.Size(40, 13);
+            this.linkLabelGitHub.TabIndex = 5;
+            this.linkLabelGitHub.TabStop = true;
+            this.linkLabelGitHub.Text = "GitHub";
+            this.linkLabelGitHub.VisitedLinkColor = System.Drawing.Color.Blue;
+            this.linkLabelGitHub.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelGitHub_LinkClicked);
+            // 
+            // linkLabelSteam
+            // 
+            this.linkLabelSteam.AutoSize = true;
+            this.linkLabelSteam.Location = new System.Drawing.Point(310, 409);
+            this.linkLabelSteam.Name = "linkLabelSteam";
+            this.linkLabelSteam.Size = new System.Drawing.Size(37, 13);
+            this.linkLabelSteam.TabIndex = 6;
+            this.linkLabelSteam.TabStop = true;
+            this.linkLabelSteam.Text = "Steam";
+            this.linkLabelSteam.VisitedLinkColor = System.Drawing.Color.Blue;
+            this.linkLabelSteam.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelSteam_LinkClicked);
+            // 
+            // textBoxServerpath
+            // 
+            this.textBoxServerpath.Location = new System.Drawing.Point(12, 75);
+            this.textBoxServerpath.Name = "textBoxServerpath";
+            this.textBoxServerpath.Size = new System.Drawing.Size(359, 20);
+            this.textBoxServerpath.TabIndex = 7;
+            // 
+            // textBoxFastDLpath
+            // 
+            this.textBoxFastDLpath.Location = new System.Drawing.Point(12, 141);
+            this.textBoxFastDLpath.Name = "textBoxFastDLpath";
+            this.textBoxFastDLpath.Size = new System.Drawing.Size(359, 20);
+            this.textBoxFastDLpath.TabIndex = 8;
+            // 
+            // btnBrowseServer
+            // 
+            this.btnBrowseServer.Location = new System.Drawing.Point(377, 75);
+            this.btnBrowseServer.Name = "btnBrowseServer";
+            this.btnBrowseServer.Size = new System.Drawing.Size(75, 20);
+            this.btnBrowseServer.TabIndex = 9;
+            this.btnBrowseServer.Text = "Browse";
+            this.btnBrowseServer.UseVisualStyleBackColor = true;
+            this.btnBrowseServer.Click += new System.EventHandler(this.btnBrowseServer_Click);
+            // 
+            // btnBrowseFastDL
+            // 
+            this.btnBrowseFastDL.Location = new System.Drawing.Point(377, 141);
+            this.btnBrowseFastDL.Name = "btnBrowseFastDL";
+            this.btnBrowseFastDL.Size = new System.Drawing.Size(75, 20);
+            this.btnBrowseFastDL.TabIndex = 10;
+            this.btnBrowseFastDL.Text = "Browse";
+            this.btnBrowseFastDL.UseVisualStyleBackColor = true;
+            this.btnBrowseFastDL.Click += new System.EventHandler(this.btnBrowseFastDL_Click);
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(12, 242);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(75, 20);
+            this.btnScan.TabIndex = 12;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(145, 242);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 20);
+            this.btnStart.TabIndex = 13;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // textBoxPrefix
+            // 
+            this.textBoxPrefix.Location = new System.Drawing.Point(12, 195);
+            this.textBoxPrefix.Name = "textBoxPrefix";
+            this.textBoxPrefix.Size = new System.Drawing.Size(62, 20);
+            this.textBoxPrefix.TabIndex = 14;
+            // 
+            // labelPrefix
+            // 
+            this.labelPrefix.AutoSize = true;
+            this.labelPrefix.Location = new System.Drawing.Point(15, 179);
+            this.labelPrefix.Name = "labelPrefix";
+            this.labelPrefix.Size = new System.Drawing.Size(56, 13);
+            this.labelPrefix.TabIndex = 15;
+            this.labelPrefix.Text = "Map prefix";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelPrefix);
+            this.Controls.Add(this.textBoxPrefix);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.btnBrowseFastDL);
+            this.Controls.Add(this.btnBrowseServer);
+            this.Controls.Add(this.textBoxFastDLpath);
+            this.Controls.Add(this.textBoxServerpath);
+            this.Controls.Add(this.linkLabelSteam);
+            this.Controls.Add(this.linkLabelGitHub);
+            this.Controls.Add(this.labelCSGOPath);
+            this.Controls.Add(this.labelFastDLPath);
+            this.Controls.Add(this.labelStatusHeader);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Quick FastDL Updater";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelStatusHeader;
+        private System.Windows.Forms.Label labelFastDLPath;
+        private System.Windows.Forms.Label labelCSGOPath;
+        private System.Windows.Forms.LinkLabel linkLabelGitHub;
+        private System.Windows.Forms.LinkLabel linkLabelSteam;
+        private System.Windows.Forms.TextBox textBoxServerpath;
+        private System.Windows.Forms.TextBox textBoxFastDLpath;
+        private System.Windows.Forms.Button btnBrowseServer;
+        private System.Windows.Forms.Button btnBrowseFastDL;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox textBoxPrefix;
+        private System.Windows.Forms.Label labelPrefix;
+    }
+}
+
---
+++ QuickFastDLUpdater/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuickFastDLUpdater
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // _Click
+        private void btnBrowseServer_Click(object sender, EventArgs e)
+        {
+            using(var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    textBoxServerpath.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void btnBrowseFastDL_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    textBoxFastDLpath.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            // Check if server textbox has text
+            if (string.IsNullOrWhiteSpace(textBoxServerpath.Text))
+            {
+                MessageBox.Show("Please check that you entered the correct server path.\n(Same folder as 'srcds.exe')", "Invalid server path", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Check if fastdl textbox has text
+            if (string.IsNullOrWhiteSpace(textBoxFastDLpath.Text))
+            {
+                MessageBox.Show("Please check if you entered a correct path to the FastDL folder.", "Invalid FastDL path", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabelSteam_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(@"http://steamcommunity.com/profiles/76561197996468677");
+        }
+
+        private void linkLabelGitHub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(@"https://github.com/HybridVenom");
+        }
+    }
+}
